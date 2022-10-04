95851d6ec21fec57b42f85f3d3313be8bc649b6f
Krzysztof Majk <funcmike@atamari.pl>
2022-10-04 20:12:30 +0200

    remove unused property

--- Sources/rabbitmq-nio/AMQPProtocol/ProtocolError.swift
+++ Sources/rabbitmq-nio/AMQPProtocol/ProtocolError.swift
@@ -1,5 +1,5 @@
 enum ProtocolError: Error {
-    case decode(type: Any.Type? = nil, value: Any? = nil, kind: Field.Kind? = nil, message: String? = nil, context: Any.Type, inner: Error? = nil)
+    case decode(type: Any.Type? = nil, kind: Field.Kind? = nil, message: String? = nil, context: Any.Type, inner: Error? = nil)
     case encode(value: Any? = nil, type: Any.Type? = nil, message: String? = nil, context: Any.Type, inner: Error? = nil)
     case invalid(value: Any,  message: String? = nil, context: Any.Type)
     case unsupported(value: Any, context: Any.Type)
